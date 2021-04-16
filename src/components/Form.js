500616b6ce8898dc0e15bea19337185859909f80
Daniel Camacho <danieluaemex@hotmail.com>
2021-04-16 10:56:03 -0500

    agrege nuevo input, reemplazando al anterior

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Error from '../components/Error'
+import axios from 'axios'
 
 /* custom hooks */
 import useLocation from '../hooks/useLocation'
@@ -8,13 +9,36 @@ const Form = () => {
     const [data, setData] = useState('')
     const [error, setError] = useState(false)
     const [Query, setOrigin] = useLocation(data)
+    const [getDataForm, setGetDataForm]=useState([])
+
+    useEffect(()=>{
+        const request = async ()=>{
+            if(!getDataForm) return
+            //const url = 'https://api.covid19api.com/live/country/mexico/status/confirmed/date/2021-04-07T00:00:00Z'
+            const date = new Date()
+            date.setDate(date.getDate() - 1)
+            const date_year = date.getFullYear()
+            const date_month = new Intl.DateTimeFormat('es', {month:'numeric'}).format(date)
+            //const date_month = date.getMonth()
+            const date_day = date.getDate()
+            //const date_day = new Intl.DateTimeFormat('es', {day:'2-digit'}).format(full_date)
+            //const date_month = new Intl.DateTimeFormat('es', {month:'numeric'}).format(full_date)
+            //const date_year = new Intl.DateTimeFormat('es', {year:'numeric'}).format(full_date)
+            const url = `https://api.covid19api.com/live/country/mexico/status/confirmed/date/${date_year}-${date_month}-${date_day}T00:00:00Z`
+            const result = await axios.get(url)
+            setGetDataForm(result.data)
+        }
+        request()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[])
+
     const getData = e =>{
         setError(false)
         const upper = e.target.value
-        const lowerLetter = upper.toLowerCase().replace(/\b[a-z]/g, (letter)=>{
-            return letter.toUpperCase()
-        })
-        setData(lowerLetter)
+        //const lowerLetter = upper.toLowerCase().replace(/\b[a-z]/g, (letter)=>{
+        //    return letter.toUpperCase()
+        //})
+        setData(upper)
         //setData(upper.charAt(0).toUpperCase() + upper.slice(1))
     }
     const submit = e=>{
@@ -31,16 +55,16 @@ const Form = () => {
             <h5 className="title is-5 has-text-centered">find information on infections in your state</h5>
             <form onSubmit={submit}>
                 <div className="field">
-                    <label htmlFor="estado">Estado</label>
-                    <div className="control">
-                        <input 
-                            className="input is-hovered is-small" 
-                            type="text" 
-                            name="state" 
-                            id="estado" 
-                            placeholder="Estado"
-                            onChange={getData}    
-                        />
+                    <div>
+                        <label htmlFor="estado">Estado</label>
+                    </div>
+                    <div className="select is-primary mt-2">
+                        <select onChange={getData} name="state" id="estado">
+                            <option value="">-- selecciona tu estado --</option>
+                            {getDataForm.map(data=>(
+                                <option key={data.ID} value={data.Province}>{data.Province}</option>
+                            ))}
+                        </select>
                     </div>
                 </div>
                 <div className="field">
